1c01e5b96be145418711175de6f4b89571aa8fee
JVH <jjvh84@gmail.com>
2022-01-19 16:09:51 -0600

    updates

--- app.js
+++ app.js
@@ -6,12 +6,21 @@ const path = require('path');
 
 const app = express();
 
-
+app.use(express.static('public'));
+app.use(express.static('img'));
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname, '/public/')));
+//app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/',(req,res)=> {
-    res.send("hello from my app");
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/Instructions',(req,res)=> {
+    res.sendFile(path.join(__dirname, '/public/Instructions.html'));
+});
+
+app.get('/studyagreement',(req,res)=> {
+    res.sendFile(path.join(__dirname, '/public/StudyAgreement.html'));
 });
 
 app.listen(3000, ()=> {
